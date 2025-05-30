dfc34864037681c4705ac36721e620daf735da8e
Tan Chang Kiat <tanchangkiat@gmail.com>
2025-05-30 07:39:01 +0000

    Added MCP server code, Dev Container config, and Readmes

---
+++ server/README.md
@@ -0,0 +1,53 @@
+# Model Context Protocol (MCP) Server Example
+
+## Using Claude Desktop
+
+### Using JS file
+
+1. Run the following commands
+
+```bash
+npm install
+npm run build
+```
+
+2. Add the MCP server in Claude Desktop configuration.
+
+```json
+{
+    "mcpServers": {
+        "singapore-weather": {
+            "command": "node",
+            "args": [
+                "/<Path to root folder of the source code>/server/build/index.js"
+            ]
+        }
+    }
+}
+```
+
+### Using Docker
+
+1. Build the Docker container image.
+
+```bash
+docker build -t mcp-server-example .
+```
+
+2. Add the MCP server in Claude Desktop configuration.
+
+```json
+{
+    "mcpServers": {
+        "singapore-weather": {
+            "command": "docker",
+            "args": [
+                "run",
+                "--rm",
+                "-i",
+                "mcp-server-example"
+            ]
+        }
+    }
+}
+```
---
+++ server/src/index.ts
@@ -0,0 +1,95 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+
+const API_BASE = "https://api-open.data.gov.sg/v2/real-time/api/air-temperature";
+const USER_AGENT = "singapore-weather-app/1.0";
+
+// Create server instance
+const server = new McpServer({
+    name: "singapore-weather",
+    version: "1.0.0",
+    capabilities: {
+        resources: {},
+        tools: {},
+    },
+});
+
+// Helper function for making API requests
+async function makeWeatherDataRequest<T>(url: string): Promise<T | null> {
+    const headers = {
+        "User-Agent": USER_AGENT,
+        Accept: "application/geo+json",
+    };
+
+    try {
+        const response = await fetch(url, { headers });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return (await response.json()) as T;
+    } catch (error) {
+        console.error("Error making request:", error);
+        return null;
+    }
+}
+
+// Interfaces are created based on the response of https://api-open.data.gov.sg/v2/real-time/api/air-temperature
+
+interface Reading {
+    timestamp: string;
+    data: ReadingData[];
+}
+
+interface ReadingData {
+    stationId?: string;
+    value?: number;
+}
+
+interface WeatherDataResponse {
+    data: {
+        readings: Reading[];
+    };
+}
+
+// Register weather tools
+server.tool(
+    "get-singapore-weather",
+    "Get weather for Singapore",
+    async () => {
+        // Get weather data
+        const weatherData = await makeWeatherDataRequest<WeatherDataResponse>(API_BASE);
+        if (!weatherData) {
+            return {
+                content: [
+                    {
+                        type: "text",
+                        text: "Failed to retrieve weather data",
+                    },
+                ],
+            };
+        }
+
+        // Format weather data
+        const weatherText = "Temperature in Singapore is " + weatherData.data.readings[0].data[0].value + "°C"
+
+        return {
+            content: [
+                {
+                    type: "text",
+                    text: weatherText,
+                },
+            ],
+        };
+    },
+);
+
+async function main() {
+    const transport = new StdioServerTransport();
+    await server.connect(transport);
+    console.error("Singapore Weather MCP Server running on stdio");
+}
+
+main().catch((error) => {
+    console.error("Fatal error in main():", error);
+    process.exit(1);
+});
